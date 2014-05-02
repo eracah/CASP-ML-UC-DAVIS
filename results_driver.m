e9b31bba7a4c77eecbec075eec4d3ecaddbd15db
Evan Racah <ejracah@gmail.com>
2014-05-02 01:45:04 -0700

    Changed implmentation, so addnewresults does the saving becasue I am still haing rouble with accesing data memebrs from within the class> i might have to inherit handle or something

--- results_driver.m
+++ results_driver.m
@@ -6,6 +6,8 @@ trainingResults = 100 * rand(1,40);
 testingResults = 100 * rand(1,10);
 trainingPermu = resultsPermu(1:40);
 testingPermu = resultsPermu(41:50);
+
+
 res = Results(labelledData);
 res.addNewResults(length(trainingResults),trainingResults, trainingPermu, testingResults, testingPermu);
 res.saveData('./Results','data.txt');
