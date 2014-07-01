d9c4c9d793dbf9abd4bc7d9a68772a118ef784d9
Evan Racah <ejracah@gmail.com>
2014-07-01 01:04:07 -0700

    This file acts as a config file. Things can easily be tweaked and easily loaded into main

--- Scikit-Learn/ReadConfigs.py
+++ Scikit-Learn/ReadConfigs.py
@@ -1,1 +1,23 @@
-__author__ = 'evanracah'
+__author__ = 'Evan Racah'
+
+import glob
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+#get list of target files
+path = './Targets/'
+
+
+test_size = 0.2
+training_sizes =[10,
+                 20,
+                 50,
+                 100,
+                 150,
+                 200,
+                 230]
+estimators = [KNeighborsRegressor(), RandomForestRegressor()]
+parameter_grids =[{'n_neighbors': [1, 2, 3, 5, 9]}, {'n_estimators': [1, 2, 4, 8, 16 ]}]
+scoring = 'mean_squared_error'
+n_folds = 5
+trials_per_size = 5
