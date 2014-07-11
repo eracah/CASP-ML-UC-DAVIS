Scatter plot for actual vs. predicted now works. Fixed the bug, which was _aggregsate_data was taking the average of the average instead of just the average, so the scatter plots were plotting only one value instead of the size of the testing set. Putting in axis=0 to the mean function does the trick

Scikit-Learn/Results.py
@@ -32,6 +32,8 @@ class TrainingSampleResult(object):
         self.fold_data = []
         self.trials = trials_per_size
         self.count = 0
+        self.test_error = 0
+        self.train_error = 0
 
 
     def add_sample_results(self, fold_data, data):
@@ -43,15 +45,19 @@ class TrainingSampleResult(object):
 
     def generate_performance_results(self, data, configs):
         num_folds = len(self.fold_data)
+        num_pred = len(self.fold_data[0].test_predicted_values)
+        num_actual = len(self.fold_data[0].test_actual_values)
         train_perf = np.ndarray(num_folds)
         test_perf = np.ndarray(num_folds)
+        test_predicted = np.ones((num_folds,num_pred,))
+        test_actual = np.ones((num_folds,num_actual,))
         for index, fold in enumerate(self.fold_data):
             # train_targets = self.train_targets[index]
             # test_targets = self.test_targets[index]
             train_predicted = fold.train_predicted_values
             train_actual = fold.train_actual_values
-            test_predicted = fold.test_predicted_values
-            test_actual = fold.test_actual_values
+            test_predicted[index] = fold.test_predicted_values
+            test_actual[index] = fold.test_actual_values
             results_loss_function = configs.results_loss_function
             train_target_ids = data.get_target_ids(fold.train_inds)
             test_target_ids = data.get_target_ids(fold.test_inds)
@@ -63,6 +69,10 @@ class TrainingSampleResult(object):
                                                                   test_actual,
                                                                   test_target_ids,
                                                                   results_loss_function)
+        self.test_actual = test_actual
+        #self.train_actual = train_actual
+        self.test_predicted = test_predicted
+        #self.train_predicted = train_predicted
         self.train_error = train_perf
         self.test_error = test_perf
 
@@ -109,9 +119,10 @@ class EstimatorResult(object):
 
     def get_aggregated_data(self, data_name, sizes):
         training_sample_results = [self.training_sample_dict[size] for size in sizes]
-        means = [getattr(obj,data_name).mean() for obj in training_sample_results]
-        vars = [getattr(obj,data_name).var() for obj in training_sample_results]
-        return means, vars
+        #must have axis=0 so we average arrays of arrays and get out an array instead of one value
+        means = [getattr(obj, data_name).mean(axis=0) for obj in training_sample_results]
+        variances = [getattr(obj, data_name).var() for obj in training_sample_results]
+        return means, variances
 
     def get_plot_arrays(self, sizes, names):
         ret = len(names)*[0]

Scikit-Learn/Visualization.py
@@ -11,7 +11,7 @@ class Visualization(object):
         self.results_obj = main_result_obj
         self.configs = configs
         self.path = self.configs.path_to_store_graphs
-        self.colors = ['r', 'b', 'g', 'y', 'k', 'm', 'c']
+        self.colors = ['r', 'g', 'y', 'k', 'm', 'c']
         self.date = self.configs.date_string
         self.color_index = 0
 
@@ -22,7 +22,7 @@ class Visualization(object):
 
     def plot_all(self):
         self.plot_learning_curve()
-        #self.plot_actual_vs_predicted_curve()
+        self.plot_actual_vs_predicted_curve()
 
     def new_color(self):
         col = self.colors[self.color_index % len(self.colors)]
@@ -30,7 +30,7 @@ class Visualization(object):
         return col
 
 
-    def scatter_data(self, estimators, sizes, x_name, y_name, alpha=1,do_scatter_plot=False):
+    def scatter_data(self, estimators, sizes, x_name, y_name, alpha=1, do_scatter_plot=False):
         legend = []
         for estimator in estimators:
             estimator_results = self.results_obj.estimator_dict[estimator]
@@ -38,7 +38,7 @@ class Visualization(object):
             if do_scatter_plot:
                 plt.scatter(x, y, c=self.new_color(), alpha=alpha)
             else:
-                plt.plot(x, y, c=self.new_color(), alpha=alpha,lw='5')
+                plt.plot(x, y, c=self.new_color(), alpha=alpha,lw=5)
             legend.append(estimator + ' ' + y_name)
         return legend
 
@@ -67,18 +67,18 @@ class Visualization(object):
     def plot_actual_vs_predicted_curve(self):
         #TODO: This doesn't work anymore.
         plot_string = 'Predicted_vs_Actual_Scatter'
-        sizes = self.result_obj.configs.training_sizes
-        #for every estimator get predictions and answers at every training size
+        sizes = self.results_obj.configs.training_sizes
         for estimator_name in self.results_obj.estimator_names:
             plt.figure(self.fig_number)
             for i, size in enumerate(sizes):
-                #get correct subplot
-                rows_of_subplot = math.ceil(math.sqrt(len(sizes)))
-                plt.subplot(rows_of_subplot*100 + rows_of_subplot*10 + i)
-                self.scatter_data([estimator_name], [size], 'test_actual_values', 'test_predicted_values',
-                                  alpha=0.01, do_scatter_plot=True)
+
+                rows_of_subplot = len(sizes)
+                plt.subplot(rows_of_subplot*100 + 4*10 + i)
+                self.scatter_data([estimator_name], [size], 'test_actual', 'test_predicted',
+                                  alpha=1, do_scatter_plot=True)
+
                 x = np.linspace(0.0, 1.0, 1000)
-                plt.plot(x, x, color=self.new_color())
+                plt.plot(x, x, color='b',linestyle='--')
 
                 title_string = estimator_name[0:3] + ' and size of ' + str(size)
                 self.set_plot_captions(title_string, 'Actual Value', 'Predicted Value')