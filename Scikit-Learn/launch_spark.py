bde0cca6cffc1c477ad3cd97c09f2e8b5c0ab562
Aubrey Gress <kickindirty@gmail.com>
2014-07-30 15:25:54 -0700

    More spark work

--- Scikit-Learn/launch_spark.py
+++ Scikit-Learn/launch_spark.py
@@ -4,6 +4,7 @@ __author__ = 'Aubrey'
 import sys
 from operator import add
 from pyspark import SparkContext
+from pyspark import SparkConf
 from main import run_main
 
 if __name__ == "__main__":
@@ -11,7 +12,12 @@ if __name__ == "__main__":
         sc
     except NameError:
         try:
-            sc = SparkContext(appName="CASP-ML")
+            spark_conf = SparkConf()
+            spark_conf.set("spark.executor.memory", "8g")
+            spark_conf.set("spark.eventLog.enable", "false")
+            spark_conf.set("spark.logConf", "true")
+            spark_conf.set("spark.default.parallelism", "2")
+            sc = SparkContext(conf=spark_conf, appName="CASP-ML")
         except:
             print "sc doesn't exist AND cannot create new SparkContext!"
             print "Did you pass globals() to execfile()?"
